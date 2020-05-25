8bc07af448cd45d72c9572d0050996e325f7a547
stdeshmukh@mitaoe.ac.in <stdeshmukh@mitaoe.ac.in>
2020-05-25 22:50:00 +0530

    Updates

---
+++ precache-manifest.61a41625f306abac2c8cb7bddde7a739.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b6782124b2af96b7b52",
+    "url": "/resume-website/static/js/runtime~main.9b678212.js"
+  },
+  {
+    "revision": "36f333511db0636d3b6f",
+    "url": "/resume-website/static/js/main.36f33351.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/resume-website/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "36f333511db0636d3b6f",
+    "url": "/resume-website/static/css/main.5695908e.chunk.css"
+  },
+  {
+    "revision": "83acd66970077c5187c11269b2f4b660",
+    "url": "/resume-website/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/sandeshdeshmukh1/resume-website/precache-manifest.a894a1940703979be92acfd8b28634d6.js"
+  "/resume-website/precache-manifest.61a41625f306abac2c8cb7bddde7a739.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/sandeshdeshmukh1/resume-website/index.html", {
+workbox.routing.registerNavigationRoute("/resume-website/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
